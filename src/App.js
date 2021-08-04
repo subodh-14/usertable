96ed1ada4c61503417fc7b6a9676ac3b7bebbca3
subodh-14 <spsubodh1999@gmail.com>
2021-08-04 22:44:34 +0530

    users app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './App.css';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import AddUsers from "./components/users/AddUsers";
+import EditUsers from './components/users/EditUsers';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+        
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/users/add" component={AddUsers} />
+            <Route exact path="/users/edit/:id" component={EditUsers} />
+            </Switch>
+      </div>
+      </Router>
   );
 }
 
